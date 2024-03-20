00aa3f795771b6c98af88535419b64e5a5c69fd9
Damian-Rios <damianjrios5@gmail.com>
2024-03-20 17:35:56 -0500

    final commit

--- model/database.php
+++ model/database.php
@@ -1,10 +1,10 @@
 <?php
-    $dsn = 'pgsql:host=user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com; port=5432; dbname=zippyusedautos-main-db-0962ebc610cabf136';
-    $username = 'zippyusedautos-main-db-0962ebc610cabf136';
-    $password = 'fXu1ae8TbdXP46wDbMnDkuQP3yrfK7';
+    $dsn = 'mysql:host=localhost; port=4306; dbname=zippyusedautos';
+    $username = 'root';
+    // $password = '1qaz2WSX';
 
 try {
-    $db = new PDO($dsn, $username, $password);
+    $db = new PDO($dsn, $username);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     $error = "Database Error: ";
